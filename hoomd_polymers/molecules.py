15345daaba46beab23ee141c9e0012f3a2b88e7c
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-03-14 17:04:46 -0600

    add doc strings to molecule classes

--- hoomd_polymers/molecules.py
+++ hoomd_polymers/molecules.py
@@ -9,6 +9,30 @@ from hoomd_polymers.library import MON_DIR
 
 
 class CoPolymer(Polymer):
+    """Builds a polymer consisting of two monomer types.
+    
+    Parameters
+    ----------
+    monomer_A : hoomd_polymers.molecules.Polymer; required
+        Class of the A-type monomer
+    monomer_B : hoomd_polymers.molecules.Polymer: required
+        Class of the B-type monomer
+    length : int; required
+        The total number of monomers in the molecule
+    sequence : str; optional; default None
+        Manually define the sequence of 'A' and 'B' monomers.
+        Leave as None if generating random sequences.
+        Example: sequence = "AABAABAAB"
+    random_sequence : bool; optional; default True
+        Creates a random 'A' 'B' sequence as a function
+        of the AB_ratio. Set to False when manually
+        defining sequence
+    AB_ratio : float; optional; default 0.50
+        The relative weight of A to B monomer types.
+        Used when generating random sequences.
+    seed : int; optional; default 24
+        Set the seed used when generating random sequences
+    """
     def __init__(
             self,
             monomer_A,
@@ -49,6 +73,13 @@ class CoPolymer(Polymer):
 
 
 class PolyEthylene(Polymer):
+    """Creates a Poly(ethylene) chain.
+
+    Parameters
+    ----------
+    length : int; required
+        The number of monomer repeat units in the chain
+    """
     def __init__(self, length):
         super(PolyEthylene, self).__init__()
         self.smiles_str = "CC"
@@ -73,6 +104,13 @@ class PolyEthylene(Polymer):
 
 
 class PPS(Polymer):
+    """Creates a Poly(phenylene-sulfide) (PPS) chain.
+
+    Parameters
+    ----------
+    length : int; required
+        The number of monomer repeat units in the chain
+    """
     def __init__(self, length):
         super(PPS, self).__init__()
         self.smiles_str = "c1ccc(S)cc1"
@@ -103,6 +141,15 @@ class PEEK(Polymer):
 
 
 class PEKK_para(Polymer):
+    """Creates a Poly(ether-ketone-ketone) (PEKK) chain.
+    The bonding positions of consecutive ketone groups
+    takes place on the para site of the phenyl ring.
+
+    Parameters
+    ----------
+    length : int; required
+        The number of monomer repeat units in the chain
+    """
     def __init__(self, length):
         super(PEKK_para, self).__init__()
         self.smiles_str = "c1ccc(Oc2ccc(C(=O)c3ccc(C(=O))cc3)cc2)cc1"
@@ -124,6 +171,15 @@ class PEKK_para(Polymer):
 
 
 class PEKK_meta(Polymer):
+    """Creates a Poly(ether-ketone-ketone) (PEKK) chain.
+    The bonding positions of consecutive ketone groups
+    takes place on the meta site of the phenyl ring.
+
+    Parameters
+    ----------
+    length : int; required
+        The number of monomer repeat units in the chain
+    """
     def __init__(self, length):
         super(PEKK_meta, self).__init__()
         self.smiles_str = "c1cc(Oc2ccc(C(=O)c3cc(C(=O))ccc3)cc2)ccc1"
@@ -145,6 +201,19 @@ class PEKK_meta(Polymer):
 
 
 class LJ_chain(mb.Compound):
+    """Creates a coarse-grained bead-spring polymer chain.
+
+    Parameters
+    ----------
+    length : int; required
+        The number of beads in the polymer chain
+    bond_length : float; required
+        The bond length between connected beads (units: nm)
+    bead_name : str; optional; default "A"
+        The name of the bead
+    bead_mass : float; optional; default 1.0
+        The mass of the bead
+    """
     def __init__(self, length, bond_length, bead_name="A", bead_mass=1.0):
         super(LJ_chain, self).__init__()
         self.description = "Simple bead-spring polymer"
