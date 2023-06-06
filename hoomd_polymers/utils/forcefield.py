5ed690432b3f953ede101c42e7469c9bee7740f0
Marjan <Marjan@cmelabs-mbp-2.boisestate.edu>
2023-06-06 16:38:33 -0600

    Update validate_hoomd_ff.

--- hoomd_polymers/utils/forcefield.py
+++ hoomd_polymers/utils/forcefield.py
@@ -3,7 +3,8 @@ import hoomd
 import forcefield_utilities as ffutils
 from gmso.parameterization import apply
 
-from hoomd_polymers.base.base_types import FF_Types
+from .base_types import FF_Types
+from .exceptions import MissingPairPotentialError
 
 FF_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../library/forcefields'))
 
@@ -45,7 +46,7 @@ def apply_xml_ff(ff_xml_path, gmso_mol):
 def _validate_hoomd_ff(forcefields, topology_information, remove_hydrogens):
     #TODO: Check if a force exsits for all bonded and non-bonded interaction types.
     pair_forces = []
-    special_pairـforces = []
+    special_pair_forces = []
     bond_forces = []
     angle_forces = []
     dihedral_forces = []
@@ -54,7 +55,7 @@ def _validate_hoomd_ff(forcefields, topology_information, remove_hydrogens):
         if isinstance(force, hoomd.md.pair.Pair):
             pair_forces.append(force)
         elif isinstance(force, hoomd.md.special_pair.SpecialPair):
-            special_pairـforces.append(force)
+            special_pair_forces.append(force)
         elif isinstance(force, hoomd.md.bond.Bond):
             bond_forces.append(force)
         elif isinstance(force, hoomd.md.angle.Angle):
@@ -62,14 +63,11 @@ def _validate_hoomd_ff(forcefields, topology_information, remove_hydrogens):
         elif isinstance(force, hoomd.md.dihedral.Dihedral):
             dihedral_forces.append(force)
 
-    for pair in topology_information["pair_types"]:
+    for f in pair_forces:
+        for pair in topology_information["pair_types"]:
+            params = list(map(list, f.params.keys()))
+            if not (list(pair) in params or list(pair[::-1]) in params):
+                raise MissingPairPotentialError(pair=tuple(pair), potential_type=type(f))
 
-        for f in pair_forces:
-            try:
-                assert (pair[0], pair[1]) in f.params.keys() or (pair[1], pair[0]) in f.params.keys()
-            except AssertionError:
-
-
-    return True
 
 
