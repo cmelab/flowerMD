7d615c64062ededc97182af2732e493a3d2bf8e3
Marjan <Marjan@cmelabs-mbp-2.boisestate.edu>
2023-06-06 16:37:23 -0600

    Add exceptions classes.

--- hoomd_polymers/utils/exceptions.py
+++ hoomd_polymers/utils/exceptions.py
@@ -3,6 +3,6 @@ class MissingPotentialError(Exception):
 
 
 class MissingPairPotentialError(MissingPotentialError):
-    def __init__(self, pair, type):
-        msg = f"Missing pair potential for {pair} in {type}"
+    def __init__(self, pair, potential_type):
+        msg = f"Missing pair potential for {pair} pair in {potential_type}."
         super().__init__(msg)
