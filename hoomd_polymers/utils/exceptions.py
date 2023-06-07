de6163259e235bf54887349bd2241c8b3e18d1e2
Marjan <Marjan@cmelabs-mbp-2.boisestate.edu>
2023-06-07 14:53:34 -0600

    Refactor exception classes.

--- hoomd_polymers/utils/exceptions.py
+++ hoomd_polymers/utils/exceptions.py
@@ -1,20 +1,35 @@
 class MissingPotentialError(Exception):
-    pass
+    def __init__(self, connection, potential_class):
+        self.connection = connection
+        self.potential_class = potential_class
+        msg = f"Missing potential for {self.connection} {self.potential_type} in {self.potential_class}."
+        super().__init__(msg)
+
+    @property
+    def potential_type(self):
+        raise NotImplementedError
 
 
 class MissingPairPotentialError(MissingPotentialError):
-    def __init__(self, pair, potential_type):
-        msg = f"Missing pair potential for {pair} pair in {potential_type}."
-        super().__init__(msg)
+    @property
+    def potential_type(self):
+        return "pair"
 
 
 class MissingBondPotentialError(MissingPotentialError):
-    def __init__(self, bond, potential_type):
-        msg = f"Missing bond potential for {bond} bond in {potential_type}."
-        super().__init__(msg)
+    @property
+    def potential_type(self):
+        return "bond"
 
 
 class MissingAnglePotentialError(MissingPotentialError):
-    def __init__(self, angle, potential_type):
-        msg = f"Missing angle potential for {angle} angle in {potential_type}."
-        super().__init__(msg)
+    @property
+    def potential_type(self):
+        return "angle"
+
+
+class MissingDihedralPotentialError(MissingPotentialError):
+    @property
+    def potential_type(self):
+        return "dihedral"
+
