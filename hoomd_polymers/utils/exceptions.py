b9758d67c9bbb3450d4a46ba94b3ae7ce20b0af0
Marjan <Marjan@cmelabs-mbp-2.boisestate.edu>
2023-06-07 11:30:21 -0600

    Add missing bond potential exception.

--- hoomd_polymers/utils/exceptions.py
+++ hoomd_polymers/utils/exceptions.py
@@ -6,3 +6,9 @@ class MissingPairPotentialError(MissingPotentialError):
     def __init__(self, pair, potential_type):
         msg = f"Missing pair potential for {pair} pair in {potential_type}."
         super().__init__(msg)
+
+
+class MissingBondPotentialError(MissingPotentialError):
+    def __init__(self, bond, potential_type):
+        msg = f"Missing bond potential for {bond} bond in {potential_type}."
+        super().__init__(msg)
