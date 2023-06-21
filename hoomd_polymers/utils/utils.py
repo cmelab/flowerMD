8c97a111748e9b00fdbd5968ef3156d5b089c2ef
Marjan <Marjan@cmelabs-MacBook-Pro-2.local>
2023-06-21 14:11:27 -0600

    Check string type in check_return_iterable.

--- hoomd_polymers/utils/utils.py
+++ hoomd_polymers/utils/utils.py
@@ -1,6 +1,8 @@
 def check_return_iterable(obj):
     if isinstance(obj, dict):
         return [obj]
+    if isinstance(obj, str):
+        return [obj]
     try:
         iter(obj)
         return obj
