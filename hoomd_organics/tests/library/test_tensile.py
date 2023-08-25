74cbe20dc502f89ae34b29644355d44bb2208731
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-08-25 14:17:41 -0600

    tensile unit test finished

--- hoomd_organics/tests/library/test_tensile.py
+++ hoomd_organics/tests/library/test_tensile.py
@@ -1,20 +1,20 @@
 import numpy as np
 
 from hoomd_organics.base.system import Lattice
-from hoomd_organics.library import OPLS_AA, Tensile
+from hoomd_organics.library import OPLS_AA_PPS, PPS, Tensile
 from hoomd_organics.tests import BaseTest
 
 
 class TestTensileSimulation(BaseTest):
-    def test_tensile(self, polyethylene):
-        polyethylene = polyethylene(lengths=5, num_mols=32)
+    def test_tensile(self):
+        pps = PPS(lengths=6, num_mols=32)
         system = Lattice(
-            molecules=[polyethylene],
-            force_field=[OPLS_AA()],
+            molecules=[pps],
+            force_field=[OPLS_AA_PPS()],
             density=1.0,
             r_cut=2.5,
-            x=10,
-            y=10,
+            x=1.2,
+            y=1.2,
             n=4,
             auto_scale=True,
         )
@@ -22,6 +22,8 @@ class TestTensileSimulation(BaseTest):
             initial_state=system.hoomd_snapshot,
             forcefield=system.hoomd_forcefield,
             tensile_axis=(1, 0, 0),
+            log_write_freq=1e6,
+            gsd_write_freq=1e6,
         )
         tensile_sim.run_tensile(strain=0.05, kT=2.0, n_steps=1e3, period=10)
         assert np.allclose(tensile_sim.strain, 0.05, 1e-4)
