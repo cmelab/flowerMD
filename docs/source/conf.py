09fa8b2ab1e16e973acadf60c068f9ad144553e4
RainierBarrett <rainier.barrett@gmail.com>
2023-09-07 15:28:54 -0600

    Restructure so conf.py is read correctly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ author = 'Chris Jones, Marjan Albooyeh, Rainier Barrett, Eric Jankowski'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
