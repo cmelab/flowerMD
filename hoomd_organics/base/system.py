cfa8602a6da1963b47d5c50424c9b3c57765a2fd
marjanalbouye <marjan.albouye73@gmail.com>
2023-09-12 17:00:06 -0600

    check if ref length is available for target box

--- hoomd_organics/base/system.py
+++ hoomd_organics/base/system.py
@@ -215,7 +215,10 @@ class System(ABC):
 
     @property
     def target_box(self):
-        return self._target_box / self.reference_length.value
+        if self.reference_length:
+            return self._target_box / self.reference_length.value
+        else:
+            return self._target_box
 
     def _remove_hydrogens(self):
         """Call this method to remove hydrogen atoms from the system.
