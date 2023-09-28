a7620b585667a2c43aa458f19a53f53a4dfafc37
marjanalbouye <marjan.albouye73@gmail.com>
2023-09-28 12:45:37 -0600

    define base forcefield classes

---
+++ hoomd_organics/base/forcefield.py
@@ -0,0 +1,30 @@
+"""Base forcefield classes."""
+import forcefield_utilities as ffutils
+import foyer
+
+from hoomd_organics.utils import FF_Types
+
+
+class BaseXMLForcefield(foyer.Forcefield):
+    """Base XML forcefield class."""
+
+    def __init__(self, forcefield_files=None, name=None):
+        super(BaseXMLForcefield, self).__init__(
+            forcefield_files=forcefield_files, name=name
+        )
+        self.ff_type = FF_Types.XML
+        self.gmso_ff = ffutils.FoyerFFs().load(forcefield_files).to_gmso_ff()
+
+
+class BaseHOOMDForcefield:
+    """Base HOOMD forcefield class."""
+
+    def __init__(self, hoomd_forces):
+        self.ff_type = FF_Types.HOOMD
+        self.hoomd_forces = hoomd_forces
+        if hoomd_forces is None:
+            raise NotImplementedError(
+                "`hoomd_forces` must be defined in the subclass."
+            )
+        if not isinstance(hoomd_forces, list):
+            raise TypeError("`hoomd_forces` must be a list.")
