4a4e93a4b421e3e0768981f16cf7c8f8a52ccec4
chrisjonesbsu <chrisjones4@u.boisestate.edu>
2023-10-03 12:50:09 -0600

    change periodicity default

--- hoomd_organics/library/surfaces.py
+++ hoomd_organics/library/surfaces.py
@@ -15,7 +15,7 @@ class Graphene(Compound):
         Number of times to repeat graphene lattice in the y-direciton
     n_layers: int, optional, default 1
         Number of times to repeat the complete layer in the normal direction
-    periodicity : tuple of bools, length=3, optional, default=None
+    periodicity : tuple of bools, length=3, optional, default=(True, True, False) # noqa: E501
         Whether the Compound is periodic in the x, y, and z directions.
         If None is provided, the periodicity is set to (False, False, False)
         which is non-periodic in all directions.
@@ -27,7 +27,9 @@ class Graphene(Compound):
 
     """
 
-    def __init__(self, x_repeat, y_repeat, n_layers, periodicity=None):
+    def __init__(
+        self, x_repeat, y_repeat, n_layers, periodicity=(True, True, False)
+    ):
         super(Graphene, self).__init__(periodicity=periodicity)
         spacings = [0.425, 0.246, 0.35]
         points = [[1 / 6, 0, 0], [1 / 2, 0, 0], [0, 0.5, 0], [2 / 3, 1 / 2, 0]]
