3d50568c485f2ba1968bed944db7b8767831c1eb
marjanalbouye <marjan.albouye73@gmail.com>
2023-11-20 12:38:54 -0700

    update combine forces utils

--- flowermd/modules/surface_wetting/utils.py
+++ flowermd/modules/surface_wetting/utils.py
@@ -1,7 +1,5 @@
 """Utility functions for the surface wetting module."""
 
-import copy
-
 import hoomd
 import numpy as np
 
@@ -12,12 +10,12 @@ def combine_forces(drop_forces, surface_forces, drop_ptypes, surface_ptypes):
 
     # retrieve individual force objects from the droplet and surface
     # force lists
-    drop_forces_dict = retrieve_forces(drop_forces)
-    surface_forces_dict = retrieve_forces(surface_forces)
+    drop_forces_dict = _retrieve_all_forces(drop_forces)
+    surface_forces_dict = _retrieve_all_forces(surface_forces)
 
     # combine LJ forces
     combined_force_list.append(
-        combine_lj_forces(
+        _combine_lj_forces(
             drop_forces_dict["lj"],
             surface_forces_dict["lj"],
             drop_ptypes,
@@ -25,31 +23,39 @@ def combine_forces(drop_forces, surface_forces, drop_ptypes, surface_ptypes):
         )
     )
 
-    # add Coulomb forces from droplet simulation if exists
+    # add Coulomb forces if exists
     if drop_forces_dict.get("coulomb"):
         combined_force_list.append(drop_forces_dict["coulomb"])
+    elif surface_forces_dict.get("coulomb"):
+        combined_force_list.append(surface_forces_dict["coulomb"])
     # combine bond forces
     combined_force_list.append(
-        combine_bond_forces(
-            drop_forces_dict["bond"], surface_forces_dict["bond"]
+        _merge_force_parameters(
+            hoomd.md.bond.Harmonic,
+            drop_forces_dict["bond"],
+            surface_forces_dict["bond"],
         )
     )
     # combine angle forces
     combined_force_list.append(
-        combine_angle_forces(
-            drop_forces_dict["angle"], surface_forces_dict["angle"]
+        _merge_force_parameters(
+            hoomd.md.angle.Harmonic,
+            drop_forces_dict["angle"],
+            surface_forces_dict["angle"],
         )
     )
     # combine dihedral forces
     combined_force_list.append(
-        combine_dihedral_forces(
-            drop_forces_dict["dihedral"], surface_forces_dict["dihedral"]
+        _merge_force_parameters(
+            hoomd.md.dihedral.OPLS,
+            drop_forces_dict["dihedral"],
+            surface_forces_dict["dihedral"],
         )
     )
     return combined_force_list
 
 
-def retrieve_forces(hoomd_force_list):
+def _retrieve_all_forces(hoomd_force_list):
     """Retrieve individual force objects from the hoomd force list."""
     force_dict = {}
     for force in hoomd_force_list:
@@ -66,7 +72,7 @@ def retrieve_forces(hoomd_force_list):
     return force_dict
 
 
-def combine_lj_forces(
+def _combine_lj_forces(
     drop_lj, surface_lj, drop_ptypes, surface_ptypes, combining_rule="geometric"
 ):
     """Combine the droplet and surface LJ forces."""
@@ -76,17 +82,17 @@ def combine_lj_forces(
     lj = hoomd.md.pair.LJ(nlist=drop_lj.nlist)
 
     # add droplet LJ parameters
-    for drop_pair, drop_params in drop_lj.params.items():
-        lj.params[drop_pair] = drop_params
-        lj.r_cut[drop_pair] = drop_lj.r_cut[drop_pair]
+    for k, v in drop_lj.params.items():
+        lj.params[k] = v
+        lj.r_cut[k] = drop_lj.r_cut[k]
 
-    # add the surface LJ parameters to the droplet LJ parameters
-    for surface_pair, surface_params in surface_lj.params.items():
-        if surface_pair not in list(lj.params.keys()):
-            lj.params[surface_pair] = surface_params
-            lj.r_cut[surface_pair] = surface_lj.r_cut[surface_pair]
+    # add the surface LJ parameters if they don't already exist
+    for k, v in surface_lj.params.items():
+        if k not in list(lj.params.keys()):
+            lj.params[k] = v
+            lj.r_cut[k] = surface_lj.r_cut[k]
 
-    # find new pairs and add them to the droplet LJ pairs
+    # add pa
     r_cut = list(drop_lj.r_cut.values())[0]
     for drop_ptype in drop_ptypes:
         for surface_ptype in surface_ptypes:
@@ -124,31 +130,14 @@ def combine_lj_forces(
     return lj
 
 
-def combine_bond_forces(drop_bond, surface_bond):
-    """Combine the droplet and surface bond forces."""
-    bond = copy.deepcopy(drop_bond)
-    # add the surface bond parameters to the droplet bond parameters
-    for k, v in surface_bond.params.items():
-        if k not in bond.params.keys():
-            bond.params[k] = v
-    return bond
-
-
-def combine_angle_forces(drop_angle, surface_angle):
-    """Combine the droplet and surface angle forces."""
-    angle = copy.deepcopy(drop_angle)
-    # add the surface angle parameters to the droplet angle parameters
-    for k, v in surface_angle.params.items():
-        if k not in angle.params.keys():
-            angle.params[k] = v
-    return angle
-
-
-def combine_dihedral_forces(drop_dihedral, surface_dihedral):
-    """Combine the droplet and surface dihedral forces."""
-    dihedral = copy.deepcopy(drop_dihedral)
-    # add the surface dihedral parameters to the droplet dihedral parameters
-    for k, v in surface_dihedral.params.items():
-        if k not in dihedral.params.keys():
-            dihedral.params[k] = v
-    return dihedral
+def _merge_force_parameters(new_force_type, drop_force_obj, surface_force_obj):
+    """Merge the force parameters of the droplet and surface forces."""
+    new_force_obj = new_force_type()
+    # add droplet force parameters
+    for k, v in drop_force_obj.params.items():
+        new_force_obj.params[k] = v
+    # add the surface force parameters if they don't already exist
+    for k, v in surface_force_obj.params.items():
+        if k not in list(new_force_obj.params.keys()):
+            new_force_obj.params[k] = v
+    return new_force_obj
