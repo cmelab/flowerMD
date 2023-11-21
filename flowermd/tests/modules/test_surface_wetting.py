887ab4ae665a94139074cc3280ce17775c13bdd5
marjanalbouye <marjan.albouye73@gmail.com>
2023-11-21 15:11:02 -0700

    add placeholder for wetting tests

---
+++ flowermd/tests/modules/test_surface_wetting.py
@@ -0,0 +1,18 @@
+# from flowermd.modules.surface_wetting import (
+#     DropletSimulation,
+#     InterfaceBuilder,
+#     WettingSimulation,
+# )
+from flowermd.tests.base_test import BaseTest
+
+
+class TestDropletSimulation(BaseTest):
+    pass
+
+
+class TestInterfaceBuilder(BaseTest):
+    pass
+
+
+class TestWettingSimulation(BaseTest):
+    pass
