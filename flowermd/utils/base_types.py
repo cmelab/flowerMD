746b85fb4ff8659976febe66cb1c6dc657a3de8f
marjanalbouye <marjan.albouye73@gmail.com>
2024-01-22 11:26:44 -0700

    add docstrings

--- flowermd/utils/base_types.py
+++ flowermd/utils/base_types.py
@@ -2,6 +2,11 @@ import hoomd
 
 
 class HOOMDThermostats:
+    """Types of HOOMD thermostats used in NVT or NPT simulations."""
+
     BERENDSEN = hoomd.md.methods.thermostats.Berendsen
+    """The Berendsen thermostat."""
     BUSSI = hoomd.md.methods.thermostats.Bussi
+    """The Bussi-Donadio-Parrinello thermostat."""
     MTTK = hoomd.md.methods.thermostats.MTTK
+    """The Nosé-Hoover thermostat."""
