6a5b7bd4a13f2307f713860dc9a6303a78ea363d
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-01-23 16:01:11 -0700

    add test for list of 2 files

--- flowermd/tests/modules/test_weld.py
+++ flowermd/tests/modules/test_weld.py
@@ -45,6 +45,44 @@ class TestWelding(BaseTest):
         if os.path.isfile("restart.gsd"):
             os.remove("restart.gsd")
 
+    def test_interface_2_files(self, polyethylene_system):
+        sim = Simulation(
+            initial_state=polyethylene_system.hoomd_snapshot,
+            forcefield=polyethylene_system.hoomd_forcefield,
+            log_write_freq=2000,
+        )
+        sim.add_walls(wall_axis=(1, 0, 0), sigma=1, epsilon=1, r_cut=2)
+        sim.run_update_volume(
+            n_steps=1000,
+            period=10,
+            kT=2.0,
+            tau_kt=0.01,
+            final_box_lengths=sim.box_lengths / 2,
+        )
+        sim.save_restart_gsd("restart.gsd")
+        sim.save_restart_gsd("restart2.gsd")
+        interface = Interface(
+            gsd_files=["restart.gsd", "restart2.gsd"],
+            interface_axis=(1, 0, 0),
+            gap=0.1,
+        )
+        interface_snap = interface.hoomd_snapshot
+        with gsd.hoomd.open("restart.gsd", "r") as traj:
+            slab_snap = traj[0]
+
+        assert interface_snap.particles.N == slab_snap.particles.N * 2
+        assert interface_snap.bonds.N == slab_snap.bonds.N * 2
+        assert interface_snap.bonds.M == slab_snap.bonds.M
+        assert interface_snap.angles.N == slab_snap.angles.N * 2
+        assert interface_snap.angles.M == slab_snap.angles.M
+        assert interface_snap.dihedrals.N == slab_snap.dihedrals.N * 2
+        assert interface_snap.dihedrals.M == slab_snap.dihedrals.M
+        assert interface_snap.pairs.N == slab_snap.pairs.N * 2
+        assert interface_snap.pairs.M == slab_snap.pairs.M
+
+        if os.path.isfile("restart.gsd"):
+            os.remove("restart.gsd")
+
     def test_slab_sim_xaxis(self, polyethylene_system):
         sim = SlabSimulation(
             initial_state=polyethylene_system.hoomd_snapshot,
