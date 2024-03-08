84f57e6b5eca60e385250f6d3972933614d929b2
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-03-08 10:03:57 -0700

    fix failing tests

--- flowermd/tests/base/test_simulation.py
+++ flowermd/tests/base/test_simulation.py
@@ -369,6 +369,11 @@ class TestSimulate(BaseTest):
                 initial_state=benzene_system.hoomd_snapshot,
                 forcefield=OPLS_AA_PPS,
             )
+        with pytest.raises(ValueError):
+            Simulation(
+                initial_state=benzene_system.hoomd_snapshot,
+                forcefield=[1, 2, 3],
+            )
 
     def test_flush(self, benzene_system):
         sim = Simulation.from_system(benzene_system, gsd_write_freq=100)
