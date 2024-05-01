b70dd7083768adb5308f2d59b5ccfb2432181720
marjanalbouye <marjan.albouye73@gmail.com>
2024-05-01 14:51:54 -0600

    add units class

--- flowermd/utils/__init__.py
+++ flowermd/utils/__init__.py
@@ -1,6 +1,7 @@
 from .actions import *
 from .base_types import HOOMDThermostats
 from .rigid_body import create_rigid_body
+from .units import Units
 from .utils import (
     _calculate_box_length,
     get_target_box_mass_density,
---
+++ flowermd/utils/units.py
@@ -0,0 +1,48 @@
+import unyt as u
+
+
+class Units:
+    # length units
+    m = u.m
+    meter = u.m
+    cm = u.cm
+    centimeter = u.cm
+    nm = u.nm
+    nanometer = u.nm
+    pm = u.pm
+    picometer = u.pm
+    ang = u.angstrom
+    angstrom = u.angstrom
+
+    # energy units
+    J = u.J
+    Joule = u.J
+    kJ = u.kJ
+    cal = u.cal
+    kcal = u.kcal
+
+    # mass units
+    g = u.g
+    gram = u.g
+    amu = u.amu
+
+    # Mol units
+    mol = u.mol
+    kcal_mol = u.kcal / u.mol
+    kJ_mol = u.kJ / u.mol
+
+    # time units
+    s = u.s
+    second = u.s
+    ns = u.ns
+    nanosecond = u.ns
+    ps = u.ps
+    picosecond = u.ps
+
+    # temperature units
+    K = u.K
+    Kelvin = u.K
+    C = u.degC
+    Celsius = u.degC
+    F = u.degF
+    Fahrenheit = u.degF
