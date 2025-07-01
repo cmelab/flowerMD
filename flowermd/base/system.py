0663fad6e406768a4b31c48ce2235ee207bed856
Steph McCallum <stephaniemccallu@u.boisestate.edu>
2025-07-01 10:41:17 -0600

    updating not logic

--- flowermd/base/system.py
+++ flowermd/base/system.py
@@ -705,15 +705,15 @@ class Pack(System):
                 f"({mass_density.dimensions}) and "
                 f"number density ({number_density.dimensions}) are supported."
             )
-        if not self.unique_molecules and len(self._molecules) > 1:
+        if self.unique_molecules==False and len(self._molecules) > 1:
             raise ValueError(
                 f"unique_molecules was set to {self.unique_molecules} "
-                "were given, which doesn't match the length of molecules given "
+                ", which doesn't match the length of molecules given "
                 f"{self._molecules}"
             )
         compound = self.all_molecules
         n_compounds = [1 for i in self.all_molecules]
-        if not self.unique_molecules:
+        if self.unique_molecules==False:
             compound = self.all_molecules[0]
             n_compounds = len(self.all_molecules)
 
