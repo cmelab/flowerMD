7e0503c0ba89fa213c9726fb443217ef5bbf8449
Steph McCallum <stephaniemccallu@u.boisestate.edu>
2025-07-07 16:30:35 -0600

    change lpar to z-axis

--- flowermd/library/polymers.py
+++ flowermd/library/polymers.py
@@ -6,7 +6,7 @@ import mbuild as mb
 import numpy as np
 from mbuild.coordinate_transform import z_axis_transform
 
-from flowermd import CoPolymer, Polymer
+from flowermd import CoPolymer, Polymer, Molecule
 from flowermd.assets import MON_DIR
 
 
@@ -316,15 +316,15 @@ class EllipsoidChain(Polymer):
         bead = mb.Compound(name="ellipsoid")
         center = mb.Compound(pos=(0, 0, 0), name="X", mass=self.bead_mass / 4)
         head = mb.Compound(
-            pos=(self.lpar + (self.bond_L / 2), 0, 0),
+            pos=(0,0,self.lpar + (self.bond_L / 2)),
             name="A",
             mass=self.bead_mass / 4,
         )
         tether_head = mb.Compound(
-            pos=(self.lpar, 0, 0), name="T", mass=self.bead_mass / 4
+            pos=(0,0,self.lpar), name="T", mass=self.bead_mass / 4
         )
         tether_tail = mb.Compound(
-            pos=(-self.lpar, 0, 0), name="T", mass=self.bead_mass / 4
+            pos=(0,0,-self.lpar), name="T", mass=self.bead_mass / 4
         )
         bead.add([center, head, tether_head, tether_tail])
         bead.add_bond([center, head])
@@ -332,7 +332,7 @@ class EllipsoidChain(Polymer):
         chain = mb.Compound()
         last_bead = None
         for i in range(length):
-            translate_by = np.array([(i * self.lpar * 2) + self.bond_L, 0, 0])
+            translate_by = np.array([0,0,(i * self.lpar * 2) + self.bond_L])
             this_bead = mb.clone(bead)
             this_bead.translate(by=translate_by)
             chain.add(this_bead)
@@ -342,3 +342,4 @@ class EllipsoidChain(Polymer):
             last_bead = this_bead
 
         return chain
+
